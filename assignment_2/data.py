e37b5333f3a79dc8df5bf0c42a009aedf1abac5d
Andrey <andrey.krutauz@gmail.com>
2023-12-18 09:41:15 -0500

    Assignment 2 code and assignment 3 prep work.

--- assignment_2/data.py
+++ assignment_2/data.py
@@ -1,15 +1,24 @@
 from typing import List
 
-from sqlalchemy import String, create_engine, select, DateTime, ForeignKey
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship, sessionmaker
+from sqlalchemy import String, create_engine, select, DateTime, ForeignKey, UniqueConstraint
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, Session, relationship
 from sqlalchemy.sql import func
-from json import JSONEncoder
+import json
+from dataclasses import dataclass
 import os
 from abc import abstractmethod, ABC
 
 
+
 class Base(DeclarativeBase):
-    pass
+    def to_dict(self):
+        """Return a dictionary representation of this model."""
+        ret_data = {}
+        columns = self.__table__.columns.keys()
+        for c in columns:
+            ret_data[c] = getattr(self, c)
+        return ret_data
+
 
 
 class User(Base):
@@ -18,11 +27,9 @@ class User(Base):
     name: Mapped[str] = mapped_column(String(50))
     tweets: Mapped[List["Tweet"]] = relationship(back_populates="user")
 
-    def to_json(self):
-        return "{}'id': {}, 'name' : '{}'{}".format('{', self.id, self.name, '}')
-
     def __repr__(self):
         return f"<User name: {self.name}>"
+
 
 
 class Tweet(Base):
@@ -36,13 +43,21 @@ class Tweet(Base):
     def __repr__(self):
         return f"<Tweet: {self.message}, Posted by: {self.user.name}, Posted on: {self.posted_on}>"
 
-    def to_json(self):
-        """
-        To implement (similar to User.to_json)
-        :return:  Json representation of object
-        """
-        pass
+class Follower(Base):
+    __tablename__ = "follower"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    followee_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    follower_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    followed_on: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    UniqueConstraint("followee_id", "follower_id", name="uix_1")
 
+class Like(Base):
+    __tablename__ = "like"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    tweet_id: Mapped[int] = mapped_column(ForeignKey("tweet.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    liked_on: Mapped[str] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    UniqueConstraint("tweet_id", "user_id", name="uix_1")
 
 class DbWrapper(ABC):
     def __init__(self):
@@ -62,48 +77,74 @@ class DbWrapper(ABC):
     def session(self):
         if self._session == None:
             self._session = Session(self._engine)
+        self._session.execute('pragma foreign_keys=on')
         return self._session
 
-    def add_user(self, name: str):
+    def add_user(self, name: str) -> User:
         user = User(name=name)
         self.session.add(user)
         self.session.commit()
+        return user
 
-    def get_users(self, filter: str):
+    def get_users(self, filter: str) -> List[User]:
         stmt = select(User)
         if not filter is None:
             stmt = select(User).filter(User.name.like(f"%{filter}%"))
-            print(stmt)
         result = self.session.execute(stmt).scalars().all()
-        json_result = []
-        for r in result:
-            json_result.append(r.to_json())
-        return json_result
+        return result
 
-    def get_user(self, id):
-        result = self.session.get(User, id)
-        if result is None:
-            return "{}"
+    def get_user(self, id) -> User:
+        user = self.session.get(User, id)
+        if user is None:
+            return User()
         else:
-            return result.to_json()
+            return user
 
-    def add_tweet(self, message: str, user_id: int) -> str:
+    def add_tweet(self, message: str, user_id: int) -> Tweet:
         """
         To implement
         :param message: Tweet message
         :param user_id: Id of posting user (should be an id from user table)
         :return: Json string with newly created tweet
         """
-        pass
+        new_tweet = Tweet()
+        new_tweet.message = message
+        new_tweet.posted_by = user_id
+        self.session.add(new_tweet)
+        self.session.commit()
+        return new_tweet
 
-    def get_tweets(self, filter: str):
+    def get_tweets(self, filter: str) -> List[Tweet]:
         """
         To implement. Return list of tweets
         :param filter: optional parameter to filter the messages
         :return: List of Json string
         """
+        stmt = select(Tweet)
+        if not filter is None:
+            stmt = select(Tweet).filter(Tweet.message.like(f"%{filter}%"))
+        result = self.session.execute(stmt).scalars().all()
+        return result
+
+    def follow(self, followee_id, follower_id):
         pass
 
+    def unfollow(self, followee_id, follower_id):
+        pass
+    def add_like(self, tweet_id, user_id):
+        pass
+
+    def remove_like(self, tweet_id, user_id):
+        pass
+
+    def get_feed(self, user_id):
+        """
+        Get user feed. Tweets posted by the followed users.
+        The most liked and the most recent on top
+        :param user_id:
+        :return:
+        """
+        pass
     def __enter__(self):
         return self
 
@@ -112,13 +153,14 @@ class DbWrapper(ABC):
 
 
 class SqlLiteDbWrapper(DbWrapper):
-
     def __init__(self, db_file_path):
         self._path = db_file_path
         super().__init__()
 
     def set_engine(self):
         self._engine = create_engine("sqlite:///" + self._path)
+
+
 
     def setup(self):
         if os.path.exists(self._path):
--- blueprints/basic_endpoints.py
+++ blueprints/basic_endpoints.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify
+from flask import Blueprint, request, json
 from mocks import DB_FILE_PATH
 from assignment_2 import SqlLiteDbWrapper
 
@@ -9,21 +9,30 @@ blueprint = Blueprint('api', __name__, url_prefix='/basic_api')
 @blueprint.route('/users/<id>')
 def get_user_by_id(id):
     with SqlLiteDbWrapper(DB_FILE_PATH) as db:
-        return db.get_user(id)
+        try:
+            return json.dumps(db.get_user(id).to_dict()), 200
+        except Exception as e:
+            print(f"Error occurred in /users/<id>. Error: {e}")
+            return "Error occurred", 500
 
 @blueprint.route('/users', methods=['GET', 'POST'])
 def users():
-    with SqlLiteDbWrapper(DB_FILE_PATH) as db:
-        if request.method == 'POST':
-            data = request.get_json()
-            name = data.get("name")
-            db.add_user(name)
-            return f"user {name} added"
-        else:
-            get_filter = request.args.get("filter")
-            print(f'filter: {get_filter}')
-            result = db.get_users(get_filter)
-            return result
+    try:
+        with SqlLiteDbWrapper(DB_FILE_PATH) as db:
+            if request.method == 'POST':
+                data = request.get_json()
+                name = data.get("name")
+                user = db.add_user(name)
+                return json.dumps(user.to_dict()), 200
+            else:
+                get_filter = request.args.get("filter")
+                print(f'filter: {get_filter}')
+                result = db.get_users(get_filter)
+                return [r.to_dict() for r in result], 200
+    except Exception as e:
+        print(f"Error occurred in /users. Error: {e}")
+        return "Error occurred", 500
+
 
 
 @blueprint.route('/tweets', methods=['GET', 'POST'])
@@ -32,8 +41,34 @@ def tweets():
     To implement
     :return:
     """
+    try:
+        with SqlLiteDbWrapper(DB_FILE_PATH) as db:
+            if request.method == 'POST':
+                data = request.get_json()
+                message = data.get("message")
+                user_id = data.get("userId")
+                tweet = db.add_tweet(message, user_id)
+                return json.dumps(tweet.to_dict()), 200
+            else:
+                get_filter = request.args.get("filter")
+                result = db.get_tweets(get_filter)
+                return [r.to_dict() for r in result], 200
+    except Exception as e:
+        print(f"Error occurred in /tweets. Error: {e}")
+        return "Error occurred", 500
+
+
+@blueprint.route('/followers/<followee_id>/<follower_id>', methods=['POST', 'DELETE'])
+def follow_unfollow_user(followee_id, follower_id):
     pass
 
+@blueprint.route('/likes/<tweet_id>/<user_id>', methods=['POST', 'DELETE'])
+def add_remove_like(tweet_id, user_id):
+    pass
+
+@blueprint.route('/feeds/<user_id>')
+def get_feed(user_id):
+    pass
 
 @blueprint.route('/hello_world')
 def hello_world():
